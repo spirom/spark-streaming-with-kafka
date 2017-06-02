20e12cea014623ce42ea66b9c94cc1ed317e416c
Spiro Michaylov <spiro@michaylov.com>
2017-06-02 07:23:39 -0700

    Kafka 0.10.0: Added ability to increase number of topic partitions on the fly.

--- src/main/scala/util/EmbeddedKafkaServer.scala
+++ src/main/scala/util/EmbeddedKafkaServer.scala
@@ -5,7 +5,9 @@ import java.net.ServerSocket
 
 import com.typesafe.scalalogging.Logger
 import info.batey.kafka.unit.KafkaUnit
-
+import kafka.admin.TopicCommand
+import kafka.utils.ZkUtils
+import org.apache.kafka.common.security.JaasUtils
 
 /**
   * Use https://github.com/chbatey/kafka-unit to control an embedded Kafka instance.
@@ -24,10 +26,32 @@ class EmbeddedKafkaServer() {
   }
 
   def createTopic(topic: String, partitions: Int = 1) {
-    LOGGER.debug(s"creating topic [$topic]")
+    LOGGER.debug(s"Creating topic [$topic]")
     kafkaServer.createTopic(topic, partitions)
-    LOGGER.debug(s"finished creating topic [$topic]")
+    LOGGER.debug(s"Finished creating topic [$topic]")
   }
+
+  def addPartitions(topic: String, partitions: Int) : Unit = {
+    LOGGER.debug(s"Adding [$partitions] partitions to [$topic]")
+
+    val arguments = Array[String](
+      "--alter",
+      "--topic",
+      topic,
+      "--partitions",
+      "" + partitions
+    )
+
+   val opts = new TopicCommand.TopicCommandOptions(arguments)
+
+    val zkUtils = ZkUtils.apply(getZkConnect,
+      30000, 30000, JaasUtils.isZkSecurityEnabled)
+
+    TopicCommand.alterTopic(zkUtils, opts)
+
+    LOGGER.debug(s"Finished adding [$partitions] partitions to [$topic]")
+  }
+
 
   def getKafkaConnect: String = "localhost:" + kbPort
 
